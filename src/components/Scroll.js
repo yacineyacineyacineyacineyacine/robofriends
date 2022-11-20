232b7aae68a25ddee813899bf9f67edf72f802b2
yacine Hadrine <yacineyacine31212@gmail.com>
2022-11-20 11:43:11 +0100

    robofriends
    robofriends

---
+++ src/components/Scroll.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Scroll = (props) => {
+
+    return (
+        <div style={{overflowY: 'scroll', border: '5px solid black', height: '1000px' }}>
+            {props.children}
+        </div>
+    )
+}
+    
+
+export default Scroll
